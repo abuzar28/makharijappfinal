1d358d4f6b474ccd171b0f4e1fd12ea97d5e3a64
abuzar.bit <abuzarrehan95@gmail.com>
2021-12-08 12:05:28 +0500

    main function and images import

--- app/src/main/java/com/example/myapplication/MainApppActivity.java
+++ app/src/main/java/com/example/myapplication/MainApppActivity.java
@@ -2,13 +2,40 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainApppActivity extends AppCompatActivity {
+
+    Button buttonPractice,buttonQuizExam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_appp);
+
+
+        buttonPractice=(Button) findViewById(R.id.btnPractice);
+        buttonQuizExam=(Button) findViewById(R.id.btnExam) ;
+
+        buttonPractice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(MainApppActivity.this, Practice.class);
+                startActivity(intent);
+
+            }
+
+        });
+        buttonQuizExam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(MainApppActivity.this, Quiz.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
