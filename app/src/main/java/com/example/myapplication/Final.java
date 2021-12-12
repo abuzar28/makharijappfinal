03c2024be7a123fdb213de4506dc9db1a5600e76
abuzar.bit <abuzarrehan95@gmail.com>
2021-12-13 00:26:47 +0500

    score sharing

--- app/src/main/java/com/example/myapplication/Final.java
+++ app/src/main/java/com/example/myapplication/Final.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Final extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class Final extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final);
+        Intent intent = getIntent();
+
+        int count = intent.getIntExtra("count",0);
+        int right = intent.getIntExtra("right",1);
+
+        TextView textView= findViewById(R.id.textViewID);
+
+        textView.setText("\n You got "+ right + " / " + count+"");
+
     }
-}
+    public void shareBtnFunction(View view){
+        Intent intent = getIntent();
+        String message;
+        message="\n You got "+ intent.getIntExtra("right",0) + " / " + intent.getIntExtra("count",1);;
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, message);
+        sendIntent.setType("text/plain");
+
+        Intent shareIntent = Intent.createChooser(sendIntent, null);
+        startActivity(shareIntent);
+    }
+    public void backMainActivity(View view){
+        Intent intent = new Intent(Final.this, MainApppActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    }
